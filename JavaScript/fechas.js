27343fec012829f7aa4f75b73057987f55509bbf
GabrielFierro237 <gabrielandro12121212@gmai.com>
2020-02-13 09:44:26 -0500

    Ejercicio en clase crear, buscar, eliminar.

---
+++ JavaScript/fechas.js
@@ -0,0 +1,34 @@
+var arreglo = [
+    {
+        nombre: 'Gabriel',
+        apellido: 'Fierro',
+        edad: 19
+    },
+];
+console.log("---Buscar---");
+var buscarUnUsuario = (arreglo, usuarioaBuscar)=>{
+    var usuarioEncontrado = arreglo.find((usuario)=>{
+        if (usuarioaBuscar === '') {
+            var agregar = arreglo.push({nombre: 'Marcela', apellido: 'Mora', edad: 66},);
+            console.log(arreglo);
+        }else{
+            return usuario.nombre === usuarioaBuscar;
+        };  
+    });
+    console.log(usuarioEncontrado);
+};
+buscarUnUsuario(arreglo, 'Gabriel');
+
+console.log("---Agregar---");
+var agregar = arreglo.push({nombre: "Alejandro", apellido: "Mora", edad: 23});
+console.log(arreglo);
+
+console.log("---Eliminar---");
+var eliminar =(arreglo,usuarioaBuscar)=>{
+    var usuarioEncontrado = arreglo.filter((usuario)=>{
+        return usuario.nombre !== usuarioaBuscar;
+    });
+    console.log(usuarioEncontrado);
+};
+eliminar(arreglo, 'Marcela');
+
